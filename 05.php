ed5cb6ab03fbbe92c23c0ac76137406d32b05ead
xuyangbao3 <434433135@qq.com>
2016-02-20 21:55:34 +0800

    wrote my first github php file

---
+++ 05.php
@@ -0,0 +1,19 @@
+﻿<?php 
+class Action{
+	public function beijing(){
+		echo 'beijing天气预报';
+	}
+
+	public function __call($m,$args){
+		echo $m,'天气预报';
+	}
+}
+
+$action = new Action();
+$method =$_GET['method'];
+
+if ($method){
+	$action->$method();
+}
+
+?>
